3eac29492cb677b276b0d3f0e6c8b279d7614d9b
Mahad <56235065+Mahad-lab@users.noreply.github.com>
2023-02-11 20:46:59 +0500

    passing data to home template

--- GeneralApp/views.py
+++ GeneralApp/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html')
+    user = {
+        'name': 'Ali'
+    }
+    user['age'] = 20
+    user['fav_fruits'] = ['Banana', 'Apple', 'Pineapple']
+
+    return render(request, 'index.html', context=user)
 
 def about(request):
     return render(request, 'about.html')
