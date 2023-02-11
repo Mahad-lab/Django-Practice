c6108ecf31acb792835b4ccca5f0ba30401c94b5
Mahad <56235065+Mahad-lab@users.noreply.github.com>
2023-02-11 19:27:55 +0500

    add 2 urls

--- GeneralApp/views.py
+++ GeneralApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME PAGE <a href="/about">about</a>')
+
+def about(request):
+    return HttpResponse('ABOUT PAGE <a href="/">Home</a>')
